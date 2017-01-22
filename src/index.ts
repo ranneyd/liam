a34cf05bc3907f8d37a6cfbc83c7a8dc0921dfeb
Sean Reimer <seanreimer@localhost.localdomain>
2017-01-21 21:49:51 -0800

    Added sass to the project, don't have watching yet

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import add from './toImport';
 
 let myAdd = function(x: number, y: number): number { return x+y; };
 
-alert(add(323, 2332333));
-alert(myAdd(2323, 2332));
+console.log(add(323, 2332333));
+console.log(myAdd(2323, 2332));
